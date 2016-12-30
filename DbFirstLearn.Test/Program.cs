902910c8303297f7323e27f7e074786d05f21dae
Neil <kengicsoftwareuploadcode@outlook.com>
2016-12-31 00:06:52 +0800

    123

--- DbFirstLearn.Test/Program.cs
+++ DbFirstLearn.Test/Program.cs
@@ -12,6 +12,58 @@ namespace DbFirstLearn.Test
         {
             db_shengtongEntities db = new db_shengtongEntities();
             var v = (from c in db.tb_inventory where c.Id == 37 select c).SingleOrDefault<tb_inventory>();
+            if (v != null)
+            {
+                Console.WriteLine("******************");
+                Console.WriteLine(v.Id);
+                Console.WriteLine(v.ConstCode);
+                Console.WriteLine("******************");
+                Console.WriteLine("******************");
+                Console.WriteLine(v.Id);
+                Console.WriteLine(v.ConstCode);
+                Console.WriteLine("******************");
+                Console.WriteLine("******************");
+                Console.WriteLine(v.Id);
+                Console.WriteLine(v.ConstCode);
+                Console.WriteLine("******************");
+                Console.WriteLine("******************");
+                Console.WriteLine(v.Id);
+                Console.WriteLine(v.ConstCode);
+                Console.WriteLine("******************");
+                Console.WriteLine("******************");
+                Console.WriteLine(v.Id);
+                Console.WriteLine(v.ConstCode);
+                Console.WriteLine("******************");
+                Console.WriteLine("******************");
+                Console.WriteLine(v.Id);
+                Console.WriteLine(v.ConstCode);
+                Console.WriteLine("******************");
+                Console.WriteLine("******************");
+                Console.WriteLine(v.Id);
+                Console.WriteLine(v.ConstCode);
+                Console.WriteLine("******************");
+                Console.WriteLine(v.Id);
+                Console.WriteLine(v.ConstCode);
+                Console.WriteLine("******************");
+                Console.WriteLine("******************");
+                Console.WriteLine(v.Id);
+                Console.WriteLine(v.ConstCode);
+                Console.WriteLine("******************");
+                Console.WriteLine("******************");
+                Console.WriteLine(v.Id);
+                Console.WriteLine(v.ConstCode);
+                Console.WriteLine("******************");
+                Console.WriteLine("******************");
+                Console.WriteLine(v.Id);
+                Console.WriteLine(v.ConstCode);
+                Console.WriteLine("******************");
+                Console.WriteLine("******************");
+                Console.WriteLine(v.Id);
+                Console.WriteLine(v.ConstCode);
+                Console.WriteLine("******************");
+                Console.WriteLine("******************");
+            }
+
 
             Console.ReadLine();
         }
